96bd2eb7ff094f8fe0060323ed5506e0b38726f4
MBStarup <mavi.nexu@gmail.com>
2022-12-07 23:17:08 +0100

    L

--- LangCore/Parser.cs
+++ LangCore/Parser.cs
@@ -1,7 +1,15 @@
 namespace Lang.LangCore;
 public static class Parser
 {
-     public static void Parse(Stack<(string type, string value)> stack) {
-        
+     public static void Parse(Stack<(string type, string value)> tokens) {
+        Stack<object> stack = new();
+        while (tokens.Count > 0) {
+            var curr = tokens.Pop();
+
+            while (true)
+            {
+                if (curr.type == "")
+            }
+        }
      } 
 }
