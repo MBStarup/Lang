814c28872874f022eb9287ca5060920ee8c6b2ea
MBStarup <mavi.nexu@gmail.com>
2022-12-07 17:17:27 +0100

    File

--- LangTool/Program.cs
+++ LangTool/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Lang.LangTool;
+
+public static class Program{
+    public static string UsageMessage = "Provide a file";
+    public static async Task<int> Main(String[] args)
+    {
+        //TODO: make, steal or use something to handle args
+        if(args.Count() < 1){
+            System.Console.Error.WriteLine(UsageMessage);
+            return 1;
+        }
+
+        using (FileStream fs = new FileStream(args[0], FileMode.Open))
+        {
+            System.Console.WriteLine("Opened File");
+        }
+
+        return 0;
+    }
+
+}
